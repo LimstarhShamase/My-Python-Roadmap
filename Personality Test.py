5b277257ac80bbec934fac2b7aefb7479cac6421
Limstarh_Shamase <129167715+LimstarhShamase@users.noreply.github.com>
2024-10-27 10:04:03 +0200

    Add files via upload

---
+++ Personality Test.py
@@ -0,0 +1,11 @@
+print("Personality test by yours truly, Limstarh_Shamase")
+answer = int(input("Q1) What are your desires\n1. Be rich af\n2.Be with my family always\n3.Have enough wisdom to last a lifetime: "))
+
+if answer == 1:
+    print("Greedy much huh?")
+elif answer == 2:
+    print("Family guy, nice")
+elif answer == 3:
+    print("Philosophical huh, nice")
+else:
+    print("wrong input")
